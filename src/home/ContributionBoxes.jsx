3e2e21b5c565080c034bba3eaeb3355bd55616df
salcedoma <salcedoma.github@gmail.com>
2025-08-18 17:28:18 -0300

    [fix] Rework component to display different pages of contribution elements.
    Create several state variables to hold current page number and contribution data to be get from the API later on.
    Use state variables to calculate page items and array indexes, to show proper content when selecting different pages.
    Import Pagination component to display buttons to navigate the different pages.

--- src/home/ContributionBoxes.jsx
+++ src/home/ContributionBoxes.jsx
@@ -1,4 +1,5 @@
 import '../styles/home/ContributionBoxes.css'
+import { useState, useEffect } from 'react';
 
 // import contributions from "../data/Contributions.json"
 import documents from "../data/Documents.json"
@@ -10,33 +11,63 @@ import {
     Link
   } from "react-router-dom";
 
-  export default function ContributionBoxes() {
+import Pagination from './Pagination';
 
-      return (
-        <>
-            {documents.slice(0,4).map((document) => (
-                <div key={document.id} className="contribution-box">
-                    <img className="contribution-icon" src={doc} alt="" />
-                    <div className="contribution-info">
-                        <h2 className="contribution-title">{document.nombre}</h2>
-                        <div className="author-and-date">
-                            <p className="contribution-author">{document.autor}</p>
-                            <p className="contribution-date">{document.fecha}</p>
-                        </div>
-                        <div className="contribution-details">
-                            <p className="contribution-type">{document.tipo}</p>
-                            <p className="contribution-downloads">{document.downloads}</p>
-                            <p className="contribution-views">{document.views}</p>
-                        </div>
+export default function ContributionBoxes() {
+    const [contributionsData, setContributionsData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    // const [itemsPerPage, setItemsPerPage] = useState(4); //In case we want to let the user change the items per page later
+
+    const itemsPerPage = 4;
+
+    // useEffect(async () => {
+    //     const response = await axios.get(
+    //         "" //API info when I get it
+    //     );
+
+    //     setContributionsData(response.data);
+    // }, []);
+
+    useEffect(() => {
+        setContributionsData(documents)
+    }, [] )
+    
+    const lastItemIndex = currentPage * itemsPerPage;
+    const firstItemIndex = lastItemIndex - itemsPerPage;
+    const currentItems = contributionsData.slice(firstItemIndex, lastItemIndex);
+
+
+    return (
+    <>
+        {currentItems.map((document) => (
+            <div key={document.id} className="contribution-box">
+                <img className="contribution-icon" src={doc} alt="" />
+                <div className="contribution-info">
+                    <h2 className="contribution-title">{document.nombre}</h2>
+                    <div className="author-and-date">
+                        <p className="contribution-author">{document.autor}</p>
+                        <p className="contribution-date">{document.fecha}</p>
                     </div>
-                    <div className="contribution-actions">
-                    <button type="button" id="see-button">Ver</button>
-                    <button type="button" id="download-button">Descargar</button>
+                    <div className="contribution-details">
+                        <p className="contribution-type">{document.tipo}</p>
+                        <p className="contribution-downloads">{document.downloads}</p>
+                        <p className="contribution-views">{document.views}</p>
                     </div>
                 </div>
-            ))}
-            
-        </>
-      );
-    }
+                <div className="contribution-actions">
+                <button type="button" id="see-button">Ver</button>
+                <button type="button" id="download-button">Descargar</button>
+                </div>
+            </div>
+        ))}
+        <Pagination
+                totalPosts={contributionsData.length}
+                postsPerPage={itemsPerPage}
+                setCurrentPage={setCurrentPage}
+                currentPage={currentPage}
+            />
+        
+    </>
+    );
+}
   
